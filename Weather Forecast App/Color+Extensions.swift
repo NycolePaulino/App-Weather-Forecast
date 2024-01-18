d3bb0d67e2d1b70c40909fe281b818c01cfdad1a
NycolePaulino <paulinonycolesantos@gmail.com>
2024-01-18 15:29:03 -0300

    Initial Commit

---
+++ Weather Forecast App/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  Weather Forecast App
+//
+//  Created by Nycole Paulino Santos on 16/01/24.
+
+import Foundation
+import UIKit
+
+//Extensão para puxar todas as cores sem precisar usar strings, oque evita erros
+extension UIColor {
+    static let contrastColor = UIColor(named: "contrastColor")
+    static let primaryColor = UIColor(named: "primaryColor")
+    static let secondColor = UIColor(named: "secondColor")
+}
---
+++ Weather Forecast App/UIView+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  UIView+Exntensions.swift
+//  Weather Forecast App
+//
+//  Created by Nycole Paulino Santos on 16/01/24.
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func setConstraintsToParent(_ parent: UIView) {
+        NSLayoutConstraint.activate([
+            self.topAnchor.constraint(equalTo: parent.topAnchor),
+            self.leadingAnchor.constraint(equalTo: parent.leadingAnchor),
+            self.trailingAnchor.constraint(equalTo: parent.trailingAnchor),
+            self.bottomAnchor.constraint(equalTo: parent.bottomAnchor)
+        ])
+    }
+}
